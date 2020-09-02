41c9882958edb75a6fb3d4bfc2a47185cde45069
Karol <appolonist@gmail.com>
2020-09-02 20:27:09 +0100

    ImgComponent added

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Header } from '../Header';
+import { ImgComponent } from '../ImgComponent';
 import './style.css';
+import img from './img.png';
 export const App = () => {
-    return <Header />
+    return (
+    <>
+    <Header />
+    <ImgComponent  src={img} alt={'tryIT'} />
+    </>
+    )
 }
---
+++ src/ImgComponent/index.js
@@ -0,0 +1,1 @@
+export { ImgComponent } from './ImgComponent.jsx'
