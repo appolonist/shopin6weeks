5770d196da0945e96e621be42ba74561657916b1
Karol <appolonist@gmail.com>
2020-09-05 18:21:23 +0100

    enzyme added and its settings in setupTest.js and App.spec.js

--- src/App/App.spec.js
+++ src/App/App.spec.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { shallow, mount } from "enzyme";
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  const wrapper = mount(<App />);
+  expect(wrapper.state("error")).toEqual(null);
 });
-
