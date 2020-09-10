34adacf8a2268796831ab69534964cabed007792
Karol <appolonist@gmail.com>
2020-09-10 20:16:42 +0100

    users API + db connection added

--- server/server.js
+++ server/server.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const jwt = require('./helpers/jwt');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/users', require('./users/user.controller'));
 
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
 const server = app.listen(port, function () {
