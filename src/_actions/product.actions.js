45db069382524982084c6daa65c1e27203136e33
Karol <appolonist@gmail.com>
2020-10-07 20:57:44 +0100

    product actions, constants and services common version added

---
+++ src/_actions/product.actions.js
@@ -0,0 +1,99 @@
+import { productConstants } from '../_constants';
+import { productService } from '../_services';
+import { alertActions } from './';
+import { history } from '../_helpers';
+
+export const productActions = {
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+};
+
+
+function create(product) {
+    return dispatch => {
+        dispatch(request(product));
+
+        productService.create(product)
+            .then(
+                product => { 
+                    dispatch(success());
+                    history.push('/products/');
+                    dispatch(alertActions.success('Product is added.'));
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    dispatch(alertActions.error(error.toString()));
+                }
+            );
+    };
+
+    function request(user) { return { type: productConstants.CREATE_REQUEST, user } }
+    function success(user) { return { type: productConstants.CREATE_SUCCESS, user } }
+    function failure(error) { return { type: productConstants.CREATE_FAILURE, error } }
+}
+
+function getAll() {
+    return dispatch => {
+        dispatch(request());
+
+        productService.getAll()
+            .then(
+                products => dispatch(success(products)),
+                error => dispatch(failure(error.toString()))
+            );
+    };
+
+    function request() { return { type: productConstants.GETALL_REQUEST } }
+    function success(products) { return { type: productConstants.GETALL_SUCCESS, users } }
+    function failure(error) { return { type: productConstants.GETALL_FAILURE, error } }
+}
+
+function getById(id) {
+    return dispatch => {
+        dispatch(request(id));
+
+        productService.getById(id)
+            .then(
+                product => dispatch(success(id)),
+                error => dispatch(failure(id, error.toString()))
+            );
+    };
+
+    function request(id) { return { type: productConstants.GET_REQUEST, id } }
+    function success(id) { return { type: productConstants.GET_SUCCESS, id } }
+    function failure(id, error) { return { type: productConstants.GET_FAILURE, id, error } }
+}
+function update(id) {
+    return dispatch => {
+        dispatch(request(id));
+
+        productService.update(id)
+            .then(
+                product => dispatch(success(id)),
+                error => dispatch(failure(id, error.toString()))
+            );
+    };
+
+    function request(id) { return { type: productConstants.UPDATE_REQUEST, id } }
+    function success(id) { return { type: productConstants.UPDATE_SUCCESS, id } }
+    function failure(id, error) { return { type: productConstants.UPDATE_FAILURE, id, error } }
+}
+
+function _delete(id) {
+    return dispatch => {
+        dispatch(request(id));
+
+        productService.delete(id)
+            .then(
+                product => dispatch(success(id)),
+                error => dispatch(failure(id, error.toString()))
+            );
+    };
+
+    function request(id) { return { type: productConstants.DELETE_REQUEST, id } }
+    function success(id) { return { type: productConstants.DELETE_SUCCESS, id } }
+    function failure(id, error) { return { type: productConstants.DELETE_FAILURE, id, error } }
+}
---
+++ src/_services/product.service.js
@@ -0,0 +1,78 @@
+import config from '../../config/config.json';
+import { authHeader } from '../_helpers';
+
+export const userService = {
+    create,
+    getAll,
+    getById,
+    update,
+    delete: _delete
+};
+
+
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/products`, requestOptions).then(handleResponse);
+}
+
+function getById(id) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/products/${id}`, requestOptions).then(handleResponse);
+}
+
+function create(product) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(product)
+    };
+
+    return fetch(`${config.apiUrl}/products/create`, requestOptions).then(handleResponse);
+}
+
+function update(product) {
+    const requestOptions = {
+        method: 'PUT',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' },
+        body: JSON.stringify(product)
+    };
+
+    return fetch(`${config.apiUrl}/products/${product.id}`, requestOptions).then(handleResponse);;
+}
+
+// prefixed function name with underscore because delete is a reserved word in javascript
+function _delete(id) {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/products/${id}`, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                logout();
+                location.reload(true);
+                console.log('After implement page for adding product go to product.service in frontend part and use history.push(/thatpage)')
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
