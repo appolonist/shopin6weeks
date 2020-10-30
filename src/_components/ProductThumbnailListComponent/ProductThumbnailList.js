b2a95835448a005036ce2db515f5847f8ca2c65d
Karol <appolonist@gmail.com>
2020-10-30 14:52:20 +0000

    Implement Lazy load to ProductThumbnailList component

--- src/_components/ProductThumbnailListComponent/ProductThumbnailList.js
+++ src/_components/ProductThumbnailListComponent/ProductThumbnailList.js
@@ -1,15 +1,16 @@
-import React, { useLayoutEffect } from 'react';
+import React from 'react';
 import style from './style.css';
 import { ProductThumbnail } from './';
-
 export function ProductThumbnailList() {
     const arr =["../../static/img/products/dressess/dress0.jpg",
                 "../../static/img/products/women/jumpers/jumper0.jpg",
                 "../../static/img/products/women/t-shirts/t-shirt0.jpg",
                 "../../static/img/products/women/hoodies/hoodie0.jpg"]
     return (
-
-        arr.map((arrUrl)=><ProductThumbnail imgUrl={arrUrl}/>)
+        <React.Suspense>
+            {arr.map((arrUrl, index)=><ProductThumbnail key={index} imgUrl={arrUrl}/>)}
+        </React.Suspense>
+        
     
     )
 }
