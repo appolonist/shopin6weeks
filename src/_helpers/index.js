41b2da39811ddf408ee4ca1cb3a74c37ff263dfd
Karol <appolonist@gmail.com>
2020-12-05 20:12:47 +0000

    added helper for app component test

--- src/_helpers/index.js
+++ src/_helpers/index.js
@@ -1,4 +1,4 @@
 export * from './history';
 export { default as store } from './store';
 export * from './auth-header';
-export { findByTestAtrr, testStore } from './testUtils';
+export { findByTestAtrr, testStore, testRender } from './testUtils';
--- src/_helpers/testUtils.js
+++ src/_helpers/testUtils.js
@@ -1,6 +1,9 @@
+import React from "react";
 import { applyMiddleware, createStore } from 'redux';
 import rootReducer from '../_reducers';
 import { middlewares } from './store';
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
 
 export const findByTestAtrr = (component, attr) => {
     const wrapper = component.find(`[data-test='${attr}']`);
@@ -11,3 +14,7 @@ export const testStore = (initialState) => {
     const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
     return createStoreWithMiddleware(rootReducer, initialState);
 };
+
+export function testRender(component, { store, ...otherOpts }) {
+    return render(<Provider store={store}>{component}</Provider>, otherOpts);
+  }
